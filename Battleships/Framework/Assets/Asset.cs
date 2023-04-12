f55134c1d8917abaa5b7f7f0c24eea0c1922cfc8
naomiEve <0xlunaric@gmail.com>
2023-04-12 21:07:23 +0200

    feat: add the concept of an asset.
    anything that can be loaded by the game at runtime will be an asset.

---
+++ Battleships/Framework/Assets/Asset.cs
@@ -0,0 +1,29 @@
+﻿namespace Battleships.Framework.Assets
+{
+    /// <summary>
+    /// A generic asset file.
+    /// </summary>
+    internal abstract class Asset
+    {
+        /// <summary>
+        /// The path to this asset.
+        /// </summary>
+        public string Path { get; private set; }
+
+        /// <summary>
+        /// Construct a new asset from the file path.
+        /// </summary>
+        /// <param name="path">The file path.</param>
+        public Asset(string path)
+        {
+            Path = path;
+            LoadFromFile(path);
+        }
+
+        /// <summary>
+        /// Load an asset from the path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        protected abstract void LoadFromFile(string path);
+    }
+}
---
+++ Battleships/Framework/Assets/Sound.cs
@@ -0,0 +1,42 @@
+﻿using Raylib_cs;
+
+namespace Battleships.Framework.Assets
+{
+    /// <summary>
+    /// A sound file.
+    /// </summary>
+    internal class Sound : Asset
+    {
+        /// <summary>
+        /// The raylib sound struct.
+        /// </summary>
+        private Raylib_cs.Sound _sound;
+
+        /// <summary>
+        /// Create a sound from the file path.
+        /// </summary>
+        /// <param name="name">The file path.</param>
+        public Sound(string path) 
+            : base(path) 
+        {
+        
+        }
+
+        /// <summary>
+        /// Load a sound from the file.
+        /// </summary>
+        /// <param name="path">The file.</param>
+        protected override void LoadFromFile(string path)
+        {
+            _sound = Raylib.LoadSound(path);
+        }
+
+        /// <summary>
+        /// Plays this sound.
+        /// </summary>
+        public void Play()
+        {
+            Raylib.PlaySoundMulti(_sound);
+        }
+    }
+}
