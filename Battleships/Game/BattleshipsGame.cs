597e82aacd111dafa73ca43be908238f9949d7cd
naomiEve <0xlunaric@gmail.com>
2023-04-17 12:52:36 +0200

    feat: add some sounds to the game.

--- Battleships/Game/BattleshipsGame.cs
+++ Battleships/Game/BattleshipsGame.cs
@@ -39,9 +39,13 @@ namespace Battleships.Game
         protected override void Start()
         {
             AssetDatabase.Load<SoundAsset>("explosion", "./assets/explosion.wav");
+            AssetDatabase.Load<SoundAsset>("seagulls", "./assets/seagulls.wav");
+
             AssetDatabase.Load<ModelAsset>("ship_head", "./assets/ship_head.obj");
             AssetDatabase.Load<ModelAsset>("ship_body", "./assets/ship_body.obj");
             AssetDatabase.Load<ModelAsset>("ship_tail", "./assets/ship_tail.obj");
+            AssetDatabase.Load<MusicAsset>("waves", "./assets/waves.ogg");
+            AssetDatabase.Load<TextureAsset>("crosshair", "./assets/crosshair.png");
 
             _camera = AddGameObject<Camera>()
                 .WithPosition(new Vector3(0, 10f, 10f))
@@ -51,7 +55,9 @@ namespace Battleships.Game
             CurrentRenderer = _camera;
 
             AddGameObject<GameCoordinator>();
-            AddGameObject<AmbienceController>();
+            var ambience = AddGameObject<AmbienceController>();
+            ambience.SetMusic(AssetDatabase.Get<MusicAsset>("waves")!);
+            ambience.CreateAmbientNoise(AssetDatabase.Get<SoundAsset>("seagulls")!, new Vector2(5f, 25f));
         }
     }
 }
