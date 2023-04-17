39d3daf6098f48eaf57fe603a84938310364a872
naomiEve <0xlunaric@gmail.com>
2023-04-17 12:52:57 +0200

    feat: add a texture asset class.

---
+++ Battleships/Framework/Assets/TextureAsset.cs
@@ -0,0 +1,22 @@
+﻿using Raylib_cs;
+
+namespace Battleships.Framework.Assets
+{
+    /// <summary>
+    /// A texture.
+    /// </summary>
+    internal class TextureAsset : Asset
+    {
+        /// <summary>
+        /// The texture.
+        /// </summary>
+        public Texture2D? Texture { get; private set; }
+
+        public override void LoadFromFile(string path)
+        {
+            Path = path;
+
+            Texture = Raylib.LoadTexture(path);
+        }
+    }
+}
